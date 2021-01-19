2509aa712e7b53e2c329ab0d698a0a7936becb3f
LappyTon <ganailyu4ok@gmail.com>
2021-01-19 22:40:17 +0200

    Module1

---
+++ Module 1/JS/task1.js
@@ -0,0 +1,15 @@
+const str = "Привіт!"; // Змінна з вмістом string
+
+const num = 123; // Змінна з вмістом number
+
+const flag = true; // Змінна з вмістом boolean
+
+const txt = "true"; // Змінна з вмістом string
+
+console.log ("Type: " + typeof str); // В консолі буде писати String
+
+console.log ("Type: " + typeof num); // В консолі буде писати Number
+
+console.log ("Type: " + typeof flag); // В консолі буде писати Boolean
+
+console.log ("Type: " + typeof txt); // В консолі буде писати String
---
+++ Module 1/JS/task2.js
@@ -0,0 +1,21 @@
+// Завдання - створити 5 let і 5 const з різними типами данних.
+
+let userName = "Ivan";
+
+let goodNumber = 3;
+
+let usersLogin = true;
+
+let userPassword = "6!#3^e#e81k@w1p!5*"; // Дуже важкий пароль
+
+let usersBestGames = 5;
+
+const colorIsYellow = false;
+
+const bestUsersEat = "ananas";
+
+const userSchoolNumber = 1;
+
+const userLikeCakes = false;
+
+const userSurname = "Kuts"
