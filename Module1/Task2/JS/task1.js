753c97feed35d621a9fff214caeca51a712f67dc
LappyTon <ganailyu4ok@gmail.com>
2021-02-08 21:03:10 +0200

    task2

---
+++ Module1/Task2/JS/task1.js
@@ -0,0 +1,87 @@
+// 1
+const enterName = prompt("Enter your name");
+
+console.log(enterName);
+
+
+// 2
+const myAge = 11;
+
+const ageMyFriend = 12;
+
+alert(myAge + ageMyFriend);
+
+
+// 3
+const a = 3;
+
+const b = 5;
+
+console.log (a + b);
+
+// 4
+let a1 = 5 + 3;
+
+let a2 = 5 - 3;
+
+let a3 = 5 * 3;
+
+let a4 = 5/3;
+
+let a5 = 5 % 3;
+
+console.log (a1);
+
+console.log (a2);
+
+console.log (a3);
+
+console.log (a4);
+
+console.log (a5);
+
+// 5
+let a6 = 5 % 3;
+
+let a7 = 3 % 5;
+
+let a8 = 5 + "3";
+
+let a9 = "5" - 3;
+
+let a10 = 75 + "кг";
+
+console.log (a6);
+
+console.log (a7);
+
+console.log (a8);
+
+console.log (a9);
+
+console.log (a10);
+
+
+// 6
+const height = 23;
+
+const width = 10;
+
+const s = height + width + " см";
+
+console.log(s);
+
+// 7
+const heightC = 10;
+const dC = 4;
+const S = 3.14;
+const v = heightC * dC * dC * S;
+console.log(v);
+
+// 8
+n = 3;
+
+m = 4;
+
+const k = Math.sqrt(n**2 + m**2);
+console.log(k)
